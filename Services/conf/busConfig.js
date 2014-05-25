03bd6ac7461ae999a8fa66ade216194ebf210220
dheesh <nidhebalan@gmail.com>
2014-05-25 11:58:32 -0500

    Added supporting angular and bootstrap libraries

--- Services/conf/busConfig.js
+++ Services/conf/busConfig.js
@@ -1,9 +1,9 @@
 var config = {
     credentials:{
-        consumer_key:"consumer key goes here",
-        consumer_secret:"consumer secret goes here",
-        token:"token goes heree",
-        token_secret:"token secret goes here"
+        consumer_key:"apBWu1Z23aSu0aVjBF6Hdw",
+        consumer_secret:"r777vr-2UtzbRLcWj03VPc2UHp8",
+        token:"MGRQ35wPR8iXtucquaTRm2Wa1vql5BgM",
+        token_secret:"fc6h9_x0TkUcZ9zQHZ11BufFqQA"
     }
 };
 
