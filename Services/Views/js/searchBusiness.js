e96c99ad5300eacfa6763571110771402dccad87
dheesh <nidhebalan@gmail.com>
2014-05-25 11:56:54 -0500

    Create toMiles filter to round off distance

--- Services/Views/js/searchBusiness.js
+++ Services/Views/js/searchBusiness.js
@@ -4,13 +4,21 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('Pagecontroller',['$scope','$http',
     function ($scope,$http){
-    window.navigator.geolocation.getCurrentPosition(function(location){
-        var latitude = location.coords.latitude;
-        var longitude = location.coords.longitude;
-        $http.get('http://localhost:3000/getTraders',
-                   {params:{lat:latitude,lon:longitude}}).
-        success(function(data){
-           $scope.items = data.businesses;
-        });
+        window.navigator.geolocation.getCurrentPosition(function(location){
+        var url = 'http://localhost:3000/getTraders';
+        var latitude = location.coords.latitude.toFixed(4);
+        var longitude = location.coords.longitude.toFixed(4);
+        $http.get(url,
+            {params:{lat:latitude,lon:longitude}})
+            .success(function(data)
+            {
+                $scope.items = data.businesses;
+            });
     });
 }]);
+
+myApp.filter('toMiles',function(){
+   return function(input){
+       return (input * 0.000621371192).toFixed(2) +' miles away';
+   };
+});
