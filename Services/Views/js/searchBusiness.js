1eeee3368deac0cb3a7f77ac93d5bbe1ef26d91f
dheesh <nidhebalan@gmail.com>
2014-05-26 15:43:04 -0500

    cache traders list,ui changes

--- Services/Views/js/searchBusiness.js
+++ Services/Views/js/searchBusiness.js
@@ -1,20 +1,28 @@
 
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp',['angular-loading-bar']);
 
 
 myApp.controller('Pagecontroller',['$scope','$http',
-    function ($scope,$http){
-        window.navigator.geolocation.getCurrentPosition(function(location){
+    function ($scope,$http,angularLoadingBar){
         var url = 'http://localhost:3000/getTraders';
-        var latitude = location.coords.latitude.toFixed(4);
-        var longitude = location.coords.longitude.toFixed(4);
-        $http.get(url,
-            {params:{lat:latitude,lon:longitude}})
-            .success(function(data)
-            {
-                $scope.items = data.businesses;
-            });
-    });
+        window.navigator.geolocation.getCurrentPosition(geoLocationSuccess,geoLocationErr);
+
+        function geoLocationSuccess(pos){
+            var latitude = pos.coords.latitude.toFixed(4);
+            var longitude = pos.coords.longitude.toFixed(4);
+
+            $http.get(url,{params:{lat:latitude,lon:longitude}})
+                 .success(function(data)
+                    {
+                        $scope.items = data.businesses;
+                        localStorage.setItem("cachedTraderList",JSON.stringify($scope.items));
+                    });
+        };
+
+        function geoLocationErr(err){
+            $scope.items = JSON.parse(localStorage.getItem("cachedTraderList"));
+            $scope.$apply();
+        };
 }]);
 
 myApp.filter('toMiles',function(){
